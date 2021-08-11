b2d3d4750ea337ca46078dedf12541fd1e262641
MatteoGevi <38760501+DataStrander@users.noreply.github.com>
2021-08-11 12:36:40 +0200

    Map file

---
+++ berlin_map_display.py
@@ -0,0 +1,31 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly
+import plotly.express as px
+from shapely.geometry import Polygon, Point
+
+
+
+
+
+# +
+
+
+# Public token to access Mapbox layout style for Plotly
+fig = px.scatter_mapbox()
